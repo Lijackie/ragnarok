c65008cea0edc9df1c87ba0602b0fc5ad0f7f859
ROProntera <jackie1234567839@gmail.com>
2020-10-30 20:36:10 +0800

    Updates

---
+++ precache-manifest.f1eb6d9fce2eb593e5dc83ec9f9731bc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1165693eae1b1882b2246257263754",
+    "url": "/ragnarok/index.html"
+  },
+  {
+    "revision": "681f0a3d4ce068de1950",
+    "url": "/ragnarok/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e7fb5db498803782e10a",
+    "url": "/ragnarok/static/css/main.031dda2a.chunk.css"
+  },
+  {
+    "revision": "681f0a3d4ce068de1950",
+    "url": "/ragnarok/static/js/2.7ad02d92.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "/ragnarok/static/js/2.7ad02d92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7fb5db498803782e10a",
+    "url": "/ragnarok/static/js/main.cd181fbd.chunk.js"
+  },
+  {
+    "revision": "860e40729ffc4d7e1fca",
+    "url": "/ragnarok/static/js/runtime-main.bbdef069.js"
+  },
+  {
+    "revision": "405e6045c5472a9193b6dffd74a8cb75",
+    "url": "/ragnarok/static/media/1.405e6045.jpg"
+  },
+  {
+    "revision": "646f4b82e7f56dd47f18c4627d02c3dc",
+    "url": "/ragnarok/static/media/2.646f4b82.jpg"
+  },
+  {
+    "revision": "cf410f99bb8cc78ee5b8e0769104f904",
+    "url": "/ragnarok/static/media/3.cf410f99.jpg"
+  },
+  {
+    "revision": "5816b4c35130ba4810b8027b34ac3375",
+    "url": "/ragnarok/static/media/4.5816b4c3.jpg"
+  }
+]);
