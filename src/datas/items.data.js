f726096c3c7f5d389b5f5e6f93f2a26c1fc59af4
ROProntera <jackie1234567839@gmail.com>
2020-10-30 22:29:27 +0800

    更新item.data路徑

--- src/datas/items.data.js
+++ src/datas/items.data.js
@@ -13,8 +13,8 @@ const ITEM_DATA = [
         name: '紅色纖細藥水',
         type: 'item',
         description: '和紅色藥水效能一樣,比它更輕。',
-        image: '/images/items/레드슬림포션.png',
-        icon: '/images/items/레드슬림포션(1).png',
+        image: 'images/items/레드슬림포션.png',
+        icon: 'images/items/레드슬림포션(1).png',
         gravity: 2
     },
     {
@@ -22,8 +22,8 @@ const ITEM_DATA = [
         name: '輕盈紅色藥水',
         type: 'item',
         description: '把紅色藥草磨粉所做成的體力恢復劑 可恢復HP量約45。',
-        image: '/images/items/빨간포션.png',
-        icon: '/images/items/빨간포션 (1).png',
+        image: 'images/items/빨간포션.png',
+        icon: 'images/items/빨간포션 (1).png',
         gravity: 1
     },
     {
@@ -31,8 +31,8 @@ const ITEM_DATA = [
         name: 'RG紅色藥水',
         type: 'item',
         description: '將紅色藥草和天地樹的汁液融合後製成的恢復藥水，可使HP恢復325左右。',
-        image: '/images/items/rg빨간포션.png',
-        icon: '/images/items/rg빨간포션 (1).png',
+        image: 'images/items/rg빨간포션.png',
+        icon: 'images/items/rg빨간포션 (1).png',
         gravity: 4
     },
     {
@@ -40,8 +40,8 @@ const ITEM_DATA = [
         name: '傭兵專用紅色藥水',
         type: 'item',
         description: '將紅色藥草和天地樹的汁液融合後製成的恢復藥水，可使HP恢復325左右。',
-        image: '/images/items/용병용빨간포션.png',
-        icon: '/images/items/용병용빨간포션 (1).png',
+        image: 'images/items/용병용빨간포션.png',
+        icon: 'images/items/용병용빨간포션 (1).png',
         gravity: 10
     },
     {
@@ -49,8 +49,8 @@ const ITEM_DATA = [
         name: '輕盈紅色藥水箱子',
         type: 'item',
         description: '裝有著輕盈紅色藥水50個的箱子, 搬運時記得要小心不要打破了。',
-        image: '/images/items/오래된보라상자.png',
-        icon: '/images/items/오래된보라상자 (1).png',
+        image: 'images/items/오래된보라상자.png',
+        icon: 'images/items/오래된보라상자 (1).png',
         gravity: 1
     },
 ]
